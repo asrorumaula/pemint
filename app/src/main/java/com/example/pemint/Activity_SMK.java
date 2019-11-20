60d8d48ab550f9d251dc714f4c28f38c0f72c525
ASRORU MAULA <asrorumaul@gmail.com>
2019-11-20 12:30:55 +0700

    first commit

---
+++ app/src/main/java/com/example/pemint/Activity_SMK.java
@@ -0,0 +1,14 @@
+package com.example.pemint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_SMK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__smk);
+    }
+}
